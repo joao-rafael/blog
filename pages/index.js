02896e0ccc791d75aa4d81770f9194e7147a584e
joao-rafael <joaorafa131@gmail.com>
2021-03-31 10:34:51 -0300

    feat(index): quick update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import matter from 'gray-matter';
 import Head from 'next/head'
 import Card from '../components/Card';
+import Link from 'next/link';
 // import fs from 'fs';
 import path from 'path';
 import styles from '../styles/Layout.module.scss'
 import fs from 'fs';
 
-export default function Home({ postProps}) {
+export default function Home({ slugs }) {
   return (
     <div>
       <Head>
@@ -26,11 +27,15 @@ export default function Home({ postProps}) {
           </picture>
         </div>
         <section className={styles.postlist}>
-          {
-            postProps.map(post => {
-              console.log(post);
-            })
-          }
+          {slugs.map(slug => {
+            return (
+              <div key={slug}>
+                <Link href={"/" + slug}>
+                  <a>{"/" + slug}</a>
+                </Link>
+              </div>
+            );
+          })}
           <Card title='Hello World!' date='March 12, 2021' description='this is my first blog post!' link='/hello'>
           </Card>
           <Card title='Making Computer Graphics Experiments' date='March 12, 2021' description='One good description' link='/hello'>
@@ -47,40 +52,16 @@ export default function Home({ postProps}) {
   );
 }
 
-export const getStaticPaths = async () => {
+export const getStaticProps = async () => {
+  const files = fs.readdirSync("posts");
 
-  const files = fs.readdirSync('posts');
-  const paths = files.map(filename => ({
-      params: {
-          slug: filename.toString()
-      }
-  }));
-  console.log(paths);
   return {
-      paths,
-      fallback: false
-  }
-}
-
-export const getStaticProps = async (paths) => {
-  
-  const postList = Object.entries(paths.params.slug);
-  console.log(postList);
-
-  const postProps = postList.map(post => {
-    const markdown =  fs.readFileSync(path.join('posts', post)).toString();
-    const parsedMarkdown = matter(markdown);
-    
-    return {
-      data: parsedMarkdown.data
+    props: {
+      slugs: files.map(filename => filename.replace(".md", ""))
     }
-  })
+  };
+};
 
-
-  return {
-    props: postProps
-  }
-}
 
 /*
  {
